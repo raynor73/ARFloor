1de0adc7664468ba65123024c7b5675da6b1282b
Igor Lapin <raynor73@gmail.com>
2016-04-29 00:38:23 +0300

    Show accelerometer direction vector

--- app/src/main/java/org/ilapin/arfloor/MainActivity.java
+++ app/src/main/java/org/ilapin/arfloor/MainActivity.java
@@ -1,13 +1,53 @@
 package org.ilapin.arfloor;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private SensorManager mSensorManager;
+
+	private NormalizedVectorView mVectorView;
+
+	private final SensorEventListener mSensorListener = new SensorEventListener() {
+
+		@Override
+		public void onSensorChanged(final SensorEvent event) {
+			mVectorView.setVector(-event.values[0], event.values[1], event.values[2]);
+		}
+
+		@Override
+		public void onAccuracyChanged(final Sensor sensor, final int accuracy) {
+			// do nothing
+		}
+	};
+	
+	@Override
+	protected void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mVectorView = (NormalizedVectorView) findViewById(R.id.view_vector);
+
+		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		final Sensor sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		mSensorManager.registerListener(mSensorListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mSensorManager.unregisterListener(mSensorListener);
+	}
 }
---
+++ app/src/main/java/org/ilapin/arfloor/NormalizedVectorView.java
@@ -0,0 +1,64 @@
+package org.ilapin.arfloor;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class NormalizedVectorView extends View {
+	private final Paint mPaint = new Paint();
+	
+	private float mNormalizedX;
+	private float mNormalizedY;
+	private float mNormalizedZ;
+	
+	private float mPixelLength;
+	private PointF mViewCenter;
+	
+	public NormalizedVectorView(final Context context) {
+		this(context, null);
+	}
+	
+	public NormalizedVectorView(final Context context, final AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
+	
+	public NormalizedVectorView(final Context context, final AttributeSet attrs, final int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		
+		mPaint.setStrokeWidth(2);
+		
+		mNormalizedX = 1;
+	}
+
+	@Override
+	protected void onSizeChanged (final int w, final int h, final int oldw, final int oldh) {
+		mPixelLength = w < h ? w / 2 : h / 2;
+		mViewCenter = new PointF(w / 2, h / 2);
+	}
+	
+	public void setVector(final float x, final float y, final float z) {
+		final double length = Math.sqrt(x * x + y * y + z * z);
+		mNormalizedX = (float) (x / length);
+		mNormalizedY = (float) (y / length);
+		mNormalizedZ = (float) (z / length);
+		
+		invalidate();
+	}
+
+	@Override
+	protected void onDraw(final Canvas canvas) {
+		if (mNormalizedZ < 0) {
+			mPaint.setColor(0xff000080);
+		} else {
+			mPaint.setColor(0xff800000);
+		}
+		final float startX = mViewCenter.x;
+		final float startY = mViewCenter.y;
+		final float endX = startX + mNormalizedX * mPixelLength;
+		final float endY = startY + mNormalizedY * mPixelLength;
+		canvas.drawLine(startX, startY, endX, endY, mPaint);
+	}
+}
